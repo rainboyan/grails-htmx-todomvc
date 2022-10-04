9bd1b52e00551f8395cd775af7b4c0391c1d940f
Michael Yan <rain@rainboyan.com>
2022-10-05 00:00:32 +0800

    Add todomvc app assets

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,7 +5,5 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better
 // to create separate JavaScript files as needed.
 //
-//= require jquery-3.5.1.min
-//= require popper.min
-//= require bootstrap
+//= require base
 //= require_self
